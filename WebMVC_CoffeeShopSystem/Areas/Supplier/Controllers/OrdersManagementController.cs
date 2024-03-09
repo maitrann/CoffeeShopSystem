6fa5389beaf64c0e9f4271437cd63f031e20d250
maitrann <truongquachmaitran@gmail.com>
2024-03-09 21:11:03 +0700

    design UI OrderSupp

---
+++ WebMVC_CoffeeShopSystem/Areas/Supplier/Controllers/OrdersManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC_CoffeeShopSystem.Areas.Supplier.Controllers
+{
+    public class OrdersManagementController : Controller
+    {
+        // GET: Supplier/Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
